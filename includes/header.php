e0092185b8379502ab8de582f00f0ecedd146c58
DNOLLE27 <damien.nolle@stadjutor.com>
2022-11-15 16:01:57 +0100

    <develop>(Version 0) : Version 0 de l'application WEB

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>site web</title>
+        <link rel="stylesheet" href="includes/css/style.css">
+    </head>
+
+    <body>
---
+++ includes/modeles/connexion.php
@@ -0,0 +1,18 @@
+<?php
+    $PARAM_hote='localhost';
+    $PARAM_port='3306';
+    $PARAM_nom_bd='';
+    $PARAM_utilisateur='root';
+    $PARAM_mot_passe='';
+
+    try
+    {
+        $bdd = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+        $bdd->exec("SET CHARACTER SET utf8");
+    }
+    catch(Exception $e)
+    {
+        echo 'Erreur : '.$e->getMessage().'<br />';
+        echo 'N° : '.$e->getCode();
+    }
+?>
