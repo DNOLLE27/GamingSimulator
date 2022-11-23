f9f62eb07a724be5a361c8c9123bd5ace160e5f4
DNOLLE27 <damien.nolle@stadjutor.com>
2022-11-23 12:30:23 +0100

    <feature>(Nav) : Réalisation du menu de navigation

--- includes/header.php
+++ includes/header.php
@@ -7,3 +7,23 @@
     </head>
 
     <body>
+        <nav class="menu-nav">
+            <table>
+                <tr>
+                    <td><img class="menu-logo" src="includes/img/logo.png"></td>
+                    <td class="td-nom-ent"><a class="nom-ent" href="#">GamingSimulator</a></td>
+                    <td>
+                        <ul>
+                            <li><a class="lien-menu" href="#">Jeux</a></li>
+                            <li>|</li>
+                            <li><a class="lien-menu" href="#">Consoles</a></li>
+                            <li>|</li>
+                            <li><a class="lien-menu" href="#">Marques</a></li>
+                            <li>|</li>
+                            <li><a class="lien-menu" href="#">Etats</a></li>
+                        </ul>
+                    </td>
+                    <td><img class="menu-deco-logo" src="includes/img/iconeCompte-bleu.png"></td>
+                </tr>
+            </table>
+        </nav>
