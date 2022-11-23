0d1bb6f3dc560f072e7cebff70b19b1e5525da68
DNOLLE27 <damien.nolle@stadjutor.com>
2022-11-23 15:09:59 +0100

    <feature>(Nav) : Fin de réalisation du menu de navigation

--- includes/header.php
+++ includes/header.php
@@ -7,23 +7,30 @@
     </head>
 
     <body>
-        <nav class="menu-nav">
-            <table>
-                <tr>
-                    <td><img class="menu-logo" src="includes/img/logo.png"></td>
-                    <td class="td-nom-ent"><a class="nom-ent" href="#">GamingSimulator</a></td>
-                    <td>
-                        <ul>
-                            <li><a class="lien-menu" href="#">Jeux</a></li>
-                            <li>|</li>
-                            <li><a class="lien-menu" href="#">Consoles</a></li>
-                            <li>|</li>
-                            <li><a class="lien-menu" href="#">Marques</a></li>
-                            <li>|</li>
-                            <li><a class="lien-menu" href="#">Etats</a></li>
-                        </ul>
-                    </td>
-                    <td><img class="menu-deco-logo" src="includes/img/iconeCompte-bleu.png"></td>
-                </tr>
-            </table>
-        </nav>
+        <div class="grid-nav">
+            <div class="grid-nav-logo">
+                <img class="menu-logo" src="includes/img/logo.png">
+            </div>
+            <div class="grid-nav-titre">
+                <a class="nom-ent" href="#">GamingSimulator</a>
+            </div>
+            <div class="grid-nav-menu">
+                <nav>
+                    <ul>
+                        <li><a class="lien-menu" href="#">Jeux</a></li>
+                        <li>|</li>
+                        <li><a class="lien-menu" href="#">Consoles</a></li>
+                        <li>|</li>
+                        <li><a class="lien-menu" href="#">Marques</a></li>
+                        <li>|</li>
+                        <li><a class="lien-menu" href="#">Etats</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="grid-nav-deco">
+                <img class="menu-deco-logo" src="includes/img/iconeCompte-bleu.png">
+            </div>
+            <div class="grid-nav-deco-texte">
+                <a class="lien-deco" href="#">Connexion</a>
+            </div>
+        </div>
