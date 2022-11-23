16ac90a688fd7ea69c0201e76926717ddc2047c5
DNOLLE27 <damien.nolle@stadjutor.com>
2022-11-23 17:12:56 +0100

    <feature>(Auth) : Page de cnx. et d'inscr. retravaillé avec le style css

--- views/v_auth.php
+++ views/v_auth.php
@@ -1,46 +1,100 @@
-<?php
-    if (!isset($_REQUEST['action']) || $_REQUEST['action'] == "inscription" || $_REQUEST['action'] == "connexion")
-    {
-        if (!verifAdminExist())
+<div class="auth-div">
+    <?php
+        if (!isset($_REQUEST['action']) || $_REQUEST['action'] == "inscription" || $_REQUEST['action'] == "connexion")
         {
-            echo '<p>Inscription de l\'administrateur</p>';
-        }
-
-        if ($typeFormulaire == "connexion")
-        {
-            echo '<form name="auth-form" id="auth-form" method="POST" action="index.php?uc=authentification&action=verifCnx">';
-        }
-        else
-        {
-            echo '<form name="auth-form" id="auth-form" method="POST" action="index.php?uc=authentification&action=verifInscr">';
-        }
-
-            echo '<input name="adrEmail" id="adrEmail" type="email" placeholder="Saisissez votre adresse e-mail" size="24" maxlength="50">';
-            if ($typeFormulaire == "inscription") { echo '<input name="adrEmailVerif" id="adrEmailVerif" type="email" placeholder="Vérifier votre adresse e-mail" size="24" maxlength="50">'; }
-            echo '<input name="mdp" id="mdp" type="password" placeholder="Saisissez votre mot de passe" size="24" maxlength="35">';
-            if ($typeFormulaire == "inscription") { echo '<input name="mdpVerif" id="mdpVerif" type="password" placeholder="Vérifier votre mot de passe" size="24" maxlength="35">'; }
-
-            if ($typeFormulaire == "inscription")
+            if ($typeFormulaire == "connexion")
             {
-                echo '<button type="submit">Inscription</button>';
+                echo '<h1>Se connecter</h1>';
             }
             else
             {
-                echo '<button type="submit">Connexion</button>';
+                echo '<h1>S\'inscrire</h1>';
             }
 
-            echo '</form>';
-    }
-    else
-    {
-        if ($messageErr != "")
-        {
-            echo '<p>'.$messageErr.'</p>';
-        }
+            if (!verifAdminExist() && isset($_REQUEST['action']) && $_REQUEST['action'] == "inscription")
+            {
+                echo '<p class="admin">Inscription de l\'administrateur</p>';
+            }
+        
+            if ($typeFormulaire == "connexion")
+            {
+                echo '<form name="auth-form" id="auth-form" method="POST" action="index.php?uc=authentification&action=verifCnx">';
+            }
+            else
+            {
+                echo '<form name="auth-form" id="auth-form" method="POST" action="index.php?uc=authentification&action=verifInscr">';
+            }
+        
+                echo '<table class="form-auth">
+                    <tr>
+                        <td><input name="adrEmail" id="adrEmail" type="email" placeholder="Saisissez votre adresse e-mail" size="24" maxlength="50"><td>
+                    </tr>';
 
-        if ($messageReussi != "")
-        {
-            echo '<p>'.$messageReussi.'</p>';
+                if ($typeFormulaire == "inscription") 
+                { 
+                    echo '<tr>
+                        <td><input name="adrEmailVerif" id="adrEmailVerif" type="email" placeholder="Vérifier votre adresse e-mail" size="24" maxlength="50"><td>
+                    </tr>'; 
+                }
+
+                echo '<tr>
+                    <td><input name="mdp" id="mdp" type="password" placeholder="Saisissez votre mot de passe" size="24" maxlength="35"><td>
+                </tr>';
+
+                if ($typeFormulaire == "inscription") 
+                { 
+                    echo '<tr>
+                        <td><input name="mdpVerif" id="mdpVerif" type="password" placeholder="Vérifier votre mot de passe" size="24" maxlength="35"><td>
+                    </tr>'; 
+                }
+            
+                if ($typeFormulaire == "inscription")
+                {
+                    echo '<tr>
+                        <td><button type="submit">Inscription</button><td>
+                    </tr>';
+                }
+                else
+                {
+                    echo '<tr>
+                        <td><button type="submit">Connexion</button><td>
+                    </tr>';
+                }
+            
+                echo '</table>
+                </form>';
+
+            if ($typeFormulaire == "connexion")
+            {
+                echo '<p>Vous n\'avez pas de compte ? <a href="index.php?uc=authentification&action=inscription">S\'inscrire</a> !</p>';
+            }
+            else
+            {
+                echo '<p>Vous avez un compte ? <a href="index.php?uc=authentification&action=connexion">Se connecter</a> !</p>';
+            }
         }
-    }
-?>
+        else
+        {
+            if (isset($messageErr) && $messageErr != "")
+            {
+                echo '<p class="msgErr">'.$messageErr.'</p>';
+                if ($_REQUEST['action'] == "verifInscr")
+                {
+                    echo '<form name="retour-form" id="retour-form" method="POST" action="index.php?uc=authentification&action=inscription">';
+                }
+                else
+                {
+                    echo '<form name="retour-form" id="retour-form" method="POST" action="index.php?uc=authentification&action=connexion">';
+                }
+
+                    echo '<button type="submit">Retour</button>
+                </form>';
+            }
+        
+            if (isset($messageReussi) && $messageReussi != "")
+            {
+                echo '<p class="msgReu">'.$messageReussi.'</p>';
+            }
+        }
+    ?>
+</div>
